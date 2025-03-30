814c88ecb817a87fc1b4f73b53bee24ec715e640
Nicolas Vazquez <vaznico93@hotmail.com>
2025-03-30 14:16:23 -0300

    creamos los archivos de components e instalamos sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter } from "react-router"
 import Rutas from "./routes/Rutas"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
     <BrowserRouter>
 
+      <Navbar />
+
       <Rutas />
+
+      <Footer />
     
     </BrowserRouter>
   )
