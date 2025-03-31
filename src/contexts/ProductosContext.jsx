82b8c81d060ec41b00295e386631b2840e100f2c
Nicolas Vazquez <vaznico93@hotmail.com>
2025-03-31 02:09:50 -0300

    finalizamos productcontext

---
+++ src/contexts/ProductosContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useEffect, useState } from "react";
+import { peticionesHttp } from "../helpers/peticiones-http";
+
+
+
+const ProductosContext = createContext()
+
+const ProductosProvider = ( { children }) => {
+
+    const url = import.meta.env.VITE_BACKEND_PRODUCTOS
+    const [productos, setProductos] = useState(null)
+
+    useEffect(() => {
+      getAllProductos()
+    }, [])
+    
+
+    const getAllProductos = async () => {
+
+        try {
+
+            const prods = await peticionesHttp(url, {})
+
+            setProductos(prods)
+
+        } catch (error) {
+            console.error('[getAllProductos]', error)
+        }
+
+    }
+
+    const data = {
+
+
+    }
+
+    return <ProductosContext.Provider value={data}> {children} </ProductosContext.Provider>
+}
+
+export { ProductosProvider }
+export default ProductosContext
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.scss'
 import App from './App.jsx'
+import { ProductosProvider } from './contexts/ProductosContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+
+    <ProductosProvider>
+      <App />
+    </ProductosProvider>
+  
   </StrictMode>,
 )
