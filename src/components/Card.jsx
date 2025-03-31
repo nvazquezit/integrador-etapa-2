b91d42397eb607aead0f00236ecbfa5caf3e3c8a
Nicolas Vazquez <vaznico93@hotmail.com>
2025-03-31 00:44:49 -0300

    agregue el archivo Card

---
+++ src/components/Card.jsx
@@ -0,0 +1,22 @@
+
+const Card = () => {
+    return (
+        <div class="card">
+            <article className="card__article">
+                <div className="card__image-container">
+                    <img className="card__image" src="foto" alt="texto foto" />
+                </div>
+                <div className="card__content">
+                    <h2 className="card__heading">Nombre</h2>
+                    <div className="card__description">
+                        <p>Precio</b></p>
+                        <p>descripcion</p>
+                        <button className="card__button">Agregar al carrito</button>
+                     </div>
+                </div>
+            </article >
+        </div >
+  )
+}
+
+export default Card
